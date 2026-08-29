034097ee594dac48be1b2bba516da61015b6f448
AndyPerezMdz <aperezmdz21@gmail.com>
2026-08-28 22:44:39 -0600

    Agregado el '<Suspense>' para el login

--- app/staff/login/page.tsx
+++ app/staff/login/page.tsx
@@ -1,5 +1,10 @@
 import { StaffLoginForm } from "@/components/staff/StaffLoginForm";
+import { Suspense } from "react";
 
 export default function StaffLoginPage() {
-  return <StaffLoginForm />;
+  return (
+    <Suspense fallback={null}>
+      <StaffLoginForm />
+    </Suspense>
+  );
 }
